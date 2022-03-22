d986f04c102be8ea36ce77fe67c1e46e71255409
rachelsola4 <101967642+rachelsola4@users.noreply.github.com>
2022-03-22 08:34:03 +0700

    Add files via upload

---
+++ metod.java
@@ -0,0 +1,17 @@
+public class metod {
+    public void judulbuku(){
+        System.out.println("judul buku yan tersedia :");
+    }
+    public void judul1() {
+        System.out.println("a). Hello panda");
+    }
+    public void judul2(){
+        System.out.println("b). Hello Jerapah");
+    }
+    public void judul3(){
+        System.out.println("c). Hello kuda");
+    }
+    public String judul (String judul){
+        return "Saya akan membeli buku berjudul : " +judul;
+    }
+}
---
+++ nyoba2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class nyoba2 {
+    public static String tulisan = "Selamat datang di Toko Buku Cemerlang!";
+
+    static void greeting(String nama){
+        System.out.println("Hai, "+nama+" selamat berbelanja!");
+    }
+
+    public static void cetakTulisan(){
+        System.out.println(tulisan);
+    }
+
+    public static void cetakNama(String buku){
+        System.out.print("Saya ingin membeli buku berjudul "+buku);
+    }
+
+    public static void hargatotal(int harga){
+        System.out.println("harga totalnya adalah "+harga);
+    }
+
+    static void ucapin(String ucapan){
+        System.out.println("Terimakasih sudah berbelanja di toko kami "+ucapan);
+    }
+
+    public static void main(String[] args){
+        greeting("rachel");
+        cetakTulisan();
+        metod objek = new metod();
+        objek.judulbuku();
+        objek.judul1();
+        objek.judul2();
+        objek.judul3();
+        String print = objek.judul("b). Hello Jerapah");
+        System.out.println(print);
+        cetakNama("Hello Jerapah sebanyak : ");
+        Scanner scan = new Scanner(System.in);
+        int i= scan.nextInt();
+        if (i==1){
+            hargatotal(50000);
+        }
+        else if (i==2){
+            hargatotal(60000);
+        }
+        else {
+            hargatotal(100000);
+        }
+        ucapin("rachel");
+    }
+}
+
+
